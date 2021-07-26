68ae52e6210e875c499fe1406cab01d18a1005d0
rokura-sys <62712875+rokura-sys@users.noreply.github.com>
2021-07-26 15:29:14 +0300

    Add files via upload

---
+++ ck-extractor/start.py
@@ -0,0 +1,40 @@
+import re ,os
+
+from telethon import TelegramClient, events,
+from .helpers import eventreciever
+
+APP_ID = int(os.environ.get("APP_ID", 12345))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN_GROUP = int(os.environ.get("ADMIN_GROUP", 12345))
+try:
+    client = TelegramClient("bot", APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
+except Exception:
+    exit()
+
+
+@client.on(events.NewMessage(pattern="/start"))
+async def _(event):
+    await eventreciever.start(event)
+
+
+@client.on(events.NewMessage(pattern="/help"))
+async def _(event):
+    await eventreciever.helps(event)
+
+@client.on(events.NewMessage(pattern="/split_everything"))
+async def _(event):
+    await eventreciever.split(event)
+
+@client.on(events.callbackquery.CallbackQuery(data=re.compile("<<")))
+async def _(event):
+    await eventreciever.start(event)
+
+
+@client.on(events.callbackquery.CallbackQuery(data=re.compile("status")))
+async def _(event):
+    await eventreciever.status(event)
+
+
+
+client.run_until_disconnected()
