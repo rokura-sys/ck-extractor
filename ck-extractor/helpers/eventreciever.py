90b6068be6d23f12d444cef16916e39c632878a9
rokura-sys <62712875+rokura-sys@users.noreply.github.com>
2021-08-08 03:39:18 -0700

    Update eventreciever.py

--- ck-extractor/helpers/eventreciever.py
+++ ck-extractor/helpers/eventreciever.py
@@ -1,10 +1,8 @@
 from telethon.tl.functions.users import GetFullUserRequest
-from .. import start
+from start import ADMIN_GROUP
 
-async def start(event):
+async def starts(event):
     user = await event.client(GetFullUserRequest(event.sender_id))
     await event.reply(
-        f"a nice stream splitter bot. but it can only be used from [Group](tg://user?id={start.ADMIN_GROUP})"
+        f"a nice stream splitter bot. but it can only be used from [Group](tg://user?id={ADMIN_GROUP})"
     )
-
-
