f2a4a044eca17d3f997a0bac027b2d63484d320f
rokura-sys <62712875+rokura-sys@users.noreply.github.com>
2021-08-08 04:09:56 -0700

    Update start.py

--- ck-extractor/start.py
+++ ck-extractor/start.py
@@ -1,18 +1,12 @@
 import re ,os
 
-from telethon import TelegramClient, events,
-from .helpers import eventreciever
+from telethon import TelegramClient, events
+from helpers import eventreciever
 
 APP_ID = int(os.environ.get("APP_ID", 12345))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN_GROUP = int(os.environ.get("ADMIN_GROUP", 12345))
-if __name__ == '__main__':
-    try:
-        client = TelegramClient("bot", APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
-    except Exception:
-        exit()
-
 
 @client.on(events.NewMessage(pattern="/start"))
 async def _(event):
@@ -36,6 +30,9 @@ async def _(event):
 async def _(event):
     await eventreciever.status(event)
 
-
 if __name__ == '__main__':
+    try:
+        client = TelegramClient("bot", APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
+    except Exception:
+        exit()
     client.run_until_disconnected()
